aede2cfac2e97fbb2b74ec97bb60ff89466cdf36
1233af <51306693+1233af@users.noreply.github.com>
2022-07-04 19:36:54 +0900

    Update database archive.py

--- database archive.py
+++ database archive.py
@@ -1,9 +1,9 @@
 import requests
 
-page_id = "4f67c16e763542af98e5359f96ab5b4f"
+page_id = "4f67c1...59f96ab5b4f"
 url = "https://api.notion.com/v1/pages/" + page_id
-database_id = '25565bb266d148feadddc82628054b21'
-TOKEN = 'secret_g1QaxRFznUeFyCoDGhddpXDOfjkiNoAV6YBwn2Kshpx'
+database_id = '25565bb26...82628054b21'
+TOKEN = 'secret_g1QaxRFznU...AV6YBwn2Kshpx'
 
 headers = {
     "Notion-Version": "2022-02-22",
